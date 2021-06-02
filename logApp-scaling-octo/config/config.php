3ce4d751fe793b1416bf49fd09ad0817ff29158d
DevineMadduma <54252782+DevineMadduma@users.noreply.github.com>
2021-06-02 17:42:19 +0800

    Add files via upload

---
+++ logApp-scaling-octo/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'guestbook-login.php');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6416541');
+	define('DB_PASS', 'NlIWLGjRas');
+	define('DB_NAME', 'sql6416541');
